2da4b77121d62d0d6ed96c9ca50cf7b1b66e9eb6
Shourjjo Majumder <shourjjomajum@outlook.com>
2023-02-04 16:44:10 +0530

    1.1.0

--- editit.c
+++ editit.c
@@ -4,7 +4,8 @@
 
 int main()
 {
-    printf("Welcome to editit\n");
+    printf("Welcome to editit 1.1.0\n");
+    printf("Made by shourdev,open source and contributions from many developers! https://github.com/shourdev/editit \n");
     printf("Enter the name of the file you want to open or create: ");
     char file_name[100];
     scanf("%s", file_name);
